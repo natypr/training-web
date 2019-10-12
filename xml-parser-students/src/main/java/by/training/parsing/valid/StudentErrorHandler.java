dbd31c93e9a4a900634c8bfb595e1e12adc9c3e4
Nataliya Statkevich <natali.vqs@gmail.com>
2019-10-12 15:33:38 +0300

    Some fix

--- xml-parser-students/src/main/java/by/training/parsing/valid/StudentErrorHandler.java
+++ xml-parser-students/src/main/java/by/training/parsing/valid/StudentErrorHandler.java
@@ -1,31 +1,38 @@
 package by.training.parsing.valid;
 
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
+
+import java.io.IOException;
+
 
 /**
- *  Обработчик ошибок.
+ * Обработчик ошибок.
  */
 public class StudentErrorHandler extends DefaultHandler {
     // создание регистратора ошибок для пакета by.bsu.valid
-    private Logger logger = Logger.getLogger("by.bsu.valid");
+    //private Logger logger = Logger.getLogger("by.bsu.valid");
+    private static Logger logger = LogManager.getLogger();
+
     public StudentErrorHandler(String log) throws IOException {
 // установка файла и формата вывода ошибок
-        logger.addAppender(new FileAppender(new SimpleLayout(), log));
+        //logger.addAppender(new FileAppender(new SimpleLayout(), log));
     }
+
     public void warning(SAXParseException e) {
         logger.warn(getLineAddress(e) + "-" + e.getMessage());
     }
+
     public void error(SAXParseException e) {
         logger.error(getLineAddress(e) + " - " + e.getMessage());
     }
+
     public void fatalError(SAXParseException e) {
         logger.fatal(getLineAddress(e) + " - " + e.getMessage());
     }
+
     private String getLineAddress(SAXParseException e) {
 // определение строки и столбца ошибки
         return e.getLineNumber() + " : " + e.getColumnNumber();
